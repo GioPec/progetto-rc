2134ed6ad32b4f39edb4b50147e2ecee2e645e02
Giovanni Pecorelli <48129769+GioPec@users.noreply.github.com>
2020-07-03 12:25:24 +0200

    s

--- app.js
+++ app.js
@@ -5,46 +5,35 @@ var express = require('express'),
   SpotifyStrategy = require('passport-spotify').Strategy,
   dotenv = require('dotenv').config();
 
-//var consolidate = require('consolidate');
-
 var app = express();
 
-// configure Express
+// configura
 app.set('views', __dirname + '/views'); //cerco le views direttamente nella cartella giusta
 app.set('view engine', 'ejs');
 
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
+// sessione
+app.use(session({ secret: process.env.cookieSegreto, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 //app.use(express.static(__dirname + '/public'));
-//app.engine('html', consolidate.ejs);
 
 //ROUTES
 
 var authRouter = require('./routes/auth');
 app.use('/', authRouter);
 
+var ricerca = require('./routes/ricerca');
+app.use('/ricerca', ricerca);
+
 //
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   //next(createError(404));
   var router = express.Router();
   module.exports = router;
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.listen(8888, () => { console.log('Server started on port 8888!')});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(8888);
+module.exports = app;
---
+++ routes/ricerca.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+  session = require('express-session'),
+  ejs = require('ejs'),
+  dotenv = require('dotenv').config(),
+  router = express.Router(),
+  got = require('got');
+//const fetch= require('node-fetch');
+
+const {ensureAuthenticated} = require('../authControl');
+const appKey = process.env.appKey;
+const appSecret = process.env.appSecret;
+
+router.get('/', function(req,res) {
+    res.send("home_film in get");
+});
+
+router.post("/", function(req, res) { 
+    var name = 'Despacito';
+
+   var url = 'https://api.spotify.com/v1/search?q='+name+'&type=track';
+    (async () => {
+      try {
+          const response = await got(url);
+          let fileJson = JSON.parse(response.body);
+          if(false){
+            res.send("La canzone che cerchi non esiste");
+          }else{
+            res.send(fileJson);
+          }
+      } catch (error) {
+          res.send(error.response.body);
+      }
+    })();
+});
+
+module.exports = router;
