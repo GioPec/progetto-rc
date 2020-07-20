a146120083e97689f6e50c0fdc6cef0550cc189a
Giovanni Pecorelli <48129769+GioPec@users.noreply.github.com>
2020-07-20 09:08:23 +0200

    pdf

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //db
+mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.mongoURL, {
   useNewUrlParser: true,
   useCreateIndex: true,
